622675684e98e107a78b6ad3c8409f2d5563c5fa
Dong Yeop Lee <dongy7@gmail.com>
2019-07-07 21:49:16 -0500

    add initial post

--- pages/2015/retail-arbitrage-with-golden-mario.js
+++ pages/2015/retail-arbitrage-with-golden-mario.js
@@ -1,15 +1,12 @@
 import Head from 'next/head'
 import Post from '../../layouts/Post'
-import RxPost from '../../markdown/2017/complex-data-fetching-made-simple-with-rx.md'
+import RxPost from '../../markdown/2015/retail-arbitrage-with-golden-mario.md'
 import withMarkdownStyle from '../../layouts/withMarkdownStyle'
 
 export default () => {
   const PostContent = withMarkdownStyle(RxPost)
   return (
-    <Post
-      title={'Complex Data Fetching Made Simple With Rx'}
-      date={'June 24, 2017'}
-    >
+    <Post title={'Retail Arbitrage with Golden Mario'} date={'March 10, 2015'}>
       <PostContent />
     </Post>
   )
